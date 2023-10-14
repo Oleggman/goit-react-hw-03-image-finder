8c63c2beaee05d4041f23d028e56ff56e5cde8a1
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-10-14 15:54:53 +0300

    Create fetch images function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { getImageByQuery } from 'services/image-api'
 
 export default class App extends Component {
+
+  async componentDidMount() {
+        const res = await getImageByQuery("cat", 1);
+        console.log(res)
+  }
+
   render() {
     return (
       <div>App</div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-import { GlobalStyle } from 'components/GlobalStyles';
+import { GlobalStyle } from 'components/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 
 const theme = {
