ddcca6e3fe4b1c420a5a954ca17af5986256abd6
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-10-14 17:19:58 +0300

    Add loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,47 +3,57 @@ import { getImageByQuery } from 'services/image-api'
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
 
 export default class App extends Component {
   state = {
     query: '',
     images: [],
-    page: 1
+    page: 1,
+    loading: false,
+    error: ''
   }
 
-  async componentDidMount() {
-
+  buildImageObj = image => {
+    return {
+      id: image.id,
+      src: image.webformatURL,
+      largeSrc: image.largeImageURL
+    }
   }
 
   async componentDidUpdate(_, prevState) {
     const { query, page } = this.state;
 
     if (prevState.query !== query) {
-      console.log(0);
-      const res = await getImageByQuery(query, 1);
-      this.setState({
-        images: res.hits.map(hit => ({
-          id: hit.id,
-          src: hit.webformatURL,
-          largeSrc: hit.largeImageURL
-        })),
-        page: 1
-      })
+      this.setState({ loading: true })
+
+      try {
+        const res = await getImageByQuery(query, 1);
+        this.setState({
+          images: res.hits.map(this.buildImageObj),
+          page: 1
+        })
+      } catch (error) {
+        this.setState({ error })
+      } finally {
+        this.setState({ loading: false })
+      }
     }
 
     if (prevState.query === query && prevState.page !== page) {
-      console.log(1);
-      const res = await getImageByQuery(query, page);
+      this.setState({ loading: true })
 
-      this.setState(prevState => ({
-        images: [...prevState.images,
-        ...res.hits.map(hit => ({
-          id: hit.id,
-          src: hit.webformatURL,
-          largeSrc: hit.largeImageURL
-        })) ]
-      })
-      )
+      try {
+        const res = await getImageByQuery(query, page);
+        this.setState(prevState => ({
+          images: [...prevState.images, ...res.hits.map(this.buildImageObj) ]
+        }))
+      } catch (error) {
+        this.setState({ error })
+      } finally {
+        this.setState({ loading: false })
+      }
     }
   }
 
@@ -57,11 +67,13 @@ export default class App extends Component {
   }
 
   render() {
-    const { images } = this.state;
+    const { images, loading } = this.state;
+
     return (
       <div>
         <Searchbar onSubmit={this.onSearchImage} />
         <ImageGallery images={this.state.images} />
+        {loading && <Loader />}
         {images.length > 0 && <Button onLoadMore={this.onLoadMore} />}
       </div>
     )
